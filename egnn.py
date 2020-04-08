be5d79d80def616ab60b0d28a08af1cb3d8eb282
xumwen <421269175@qq.com>
2020-04-08 23:08:23 +0800

    add GatedGCN and supports sample

--- egnn.py
+++ egnn.py
@@ -3,12 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch_geometric.nn as PyG
 from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.utils import add_remaining_self_loops
 
 class SAGELA(PyG.SAGEConv):
     def __init__(self, in_channels, out_channels, edge_channels,
                  normalize=False, concat=True, bias=True, **kwargs):
-        super(SAGELA, self).__init__(in_channels, out_channels, 
-                                         normalize=normalize, concat=concat, bias=bias, **kwargs)
+        super(SAGELA, self).__init__(in_channels, out_channels, normalize=normalize,
+                                     concat=concat, bias=bias, **kwargs)
+
         self.edge_channels = edge_channels
         self.amp_weight = nn.Parameter(torch.Tensor(edge_channels, in_channels))
         self.gate_linear = nn.Linear(2 * in_channels + edge_channels, 1)
@@ -16,10 +18,6 @@ class SAGELA(PyG.SAGEConv):
 
     def forward(self, x, edge_index, edge_feature, size=None,
                 res_n_id=None):
-        if not self.concat and torch.is_tensor(x):
-            edge_index, edge_weight = add_remaining_self_loops(
-                edge_index, edge_weight, 1, x.size(self.node_dim))
-
         return self.propagate(edge_index, size=size, x=x,
                               edge_feature=edge_feature, res_n_id=res_n_id)
 
@@ -30,7 +28,7 @@ class SAGELA(PyG.SAGEConv):
 
         # amplifier
         amp = torch.matmul(edge_feature, self.amp_weight)
-        amp_x_j = amp.view(1, -1, self.in_channels) * x_j
+        amp_x_j = amp * x_j
 
         return amp_x_j * lamb
 
@@ -76,5 +74,82 @@ class SAGELANet(nn.Module):
             (X, X[:, res_n_id[1]]), edge_index[1], edge_feature=edge_weight[1].unsqueeze(-1), size=size[1])
 
         X = F.leaky_relu(conv2)
+
+        return X
+
+
+class GatedGCN(MessagePassing):
+    def __init__(self, in_channels, out_channels, edge_channels, 
+                 **kwargs):
+        super(GatedGCN, self).__init__(aggr='add', **kwargs)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.edge_channels = edge_channels
+        
+        self.weight1 = nn.Parameter(torch.Tensor(in_channels, out_channels))
+        self.weight2 = nn.Parameter(torch.Tensor(edge_channels, out_channels))
+
+        self.u = nn.Parameter(torch.Tensor(out_channels, out_channels))
+        self.v = nn.Parameter(torch.Tensor(out_channels, out_channels))
+
+        self.reset_parameters()
+    
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.weight1)
+        nn.init.xavier_uniform_(self.weight2)
+        nn.init.xavier_uniform_(self.u)
+        nn.init.xavier_uniform_(self.v)
+
+    def forward(self, x, edge_index, edge_feature, size=None,
+                res_n_id=None):
+        x = torch.matmul(x, self.weight1)
+        edge_emb = torch.matmul(edge_feature, self.weight2)
+
+        return self.propagate(edge_index, size=size, x=x,
+                              edge_emb=edge_emb, res_n_id=res_n_id)
+
+    def message(self, x_j, edge_emb):
+        x_j = torch.matmul(x_j, self.v)
+
+        return edge_emb * x_j
+
+    def update(self, aggr_out, x, res_n_id):
+        if res_n_id is not None:
+            x = x[:, res_n_id]
+
+        aggr_out = torch.matmul(x, self.u) + aggr_out
+
+        bn = nn.BatchNorm1d(aggr_out.shape[1]).to(x.device)
+        aggr_out = F.relu(bn(aggr_out))
+        
+        aggr_out = x + aggr_out
+        
+        return aggr_out
+
+
+class GatedGCNNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(GatedGCNNet, self).__init__()
+        self.conv1 = GatedGCN(
+            in_channels, 16, edge_channels=1, node_dim=1)
+        self.conv2 = GatedGCN(
+            16, out_channels, edge_channels=1, node_dim=1)
+
+    def forward(self, X, g):
+        edge_index = g['edge_index']
+        edge_weight = g['edge_weight']
+
+        size = g['size']
+        n_id = g['graph_n_id']
+        res_n_id = g['res_n_id']
+        
+        X = X[:, n_id]
+
+        X = self.conv1(
+            X, edge_index[0], edge_feature=edge_weight[0].unsqueeze(-1), size=size[0], res_n_id=res_n_id[0])
+
+        X = self.conv2(
+            X, edge_index[1], edge_feature=edge_weight[1].unsqueeze(-1), size=size[1], res_n_id=res_n_id[1])
 
         return X
--- tgcn.py
+++ tgcn.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from gcn import SAGENet, GATNet
-from egnn import SAGELANet
+from egnn import SAGELANet, GatedGCNNet
 from krnn import KRNN
 
 from torch_geometric.data import Data, Batch, DataLoader, NeighborSampler, ClusterData, ClusterLoader
@@ -13,7 +13,8 @@ class GCNBlock(nn.Module):
     def __init__(self, in_channels, spatial_channels, num_nodes,
                  gcn_type, gcn_partition):
         super(GCNBlock, self).__init__()
-        GCNUnit = {'sage': SAGENet, 'gat': GATNet, 'sagela': SAGELANet}.get(gcn_type)
+        GCNUnit = {'sage': SAGENet, 'gat': GATNet, 
+                'sagela': SAGELANet, 'gated': GatedGCNNet}.get(gcn_type)
         self.gcn = GCNUnit(in_channels=in_channels,
                            out_channels=spatial_channels)
 
