c55346ac4af85f158066a91dc113e8316e40e3ad
xumwen <421269175@qq.com>
2020-04-13 17:31:17 +0800

    fix bug

--- stgcn.py
+++ stgcn.py
@@ -73,9 +73,8 @@ class STGCNBlock(nn.Module):
                            out_channels=spatial_channels)
         self.temporal2 = TimeBlock(in_channels=spatial_channels,
                                    out_channels=out_channels)
-        self.batch_norm = nn.BatchNorm2d(num_nodes)
 
-    def forward(self, X, A, edge_index, edge_weight):
+    def forward(self, X, g):
         """
         :param X: Input data of shape (batch_size, num_nodes, num_timesteps,
         num_features=in_channels).
@@ -86,12 +85,12 @@ class STGCNBlock(nn.Module):
         t1 = self.temporal1(X)
         # batch_size * timesteps -> batch_size
         t21 = t1.permute(0, 2, 1, 3).contiguous().view(-1, t1.shape[1], t1.shape[3])
-        t22 = F.relu(self.gcn(t21, A, edge_index, edge_weight))
+        t22 = F.relu(self.gcn(t21, g))
         # batch_size -> (batch_size, timesteps)
         t23 = t22.view(t1.shape[0], t1.shape[2], t22.shape[1], t22.shape[2]).permute(0, 2, 1, 3)
         t3 = self.temporal2(t23)
         
-        return self.batch_norm(t3)
+        return t3
 
 
 class STGCN(nn.Module):
@@ -127,14 +126,14 @@ class STGCN(nn.Module):
         self.fully = nn.Linear((num_timesteps_input - 2 * 5) * 64,
                                num_timesteps_output)
 
-    def forward(self, X, A=None, edge_index=None, edge_weight=None):
+    def forward(self, X, g):
         """
         :param X: Input data of shape (batch_size, num_nodes, num_timesteps,
         num_features=in_channels).
         :param A: Normalized adjacency matrix.
         """
-        out1 = self.block1(X, A, edge_index, edge_weight)
-        out2 = self.block2(out1, A, edge_index, edge_weight)
+        out1 = self.block1(X, g)
+        out2 = self.block2(out1, g)
         out3 = self.last_temporal(out2)
         out4 = self.fully(out3.reshape((out3.shape[0], out3.shape[1], -1)))
         return out4
