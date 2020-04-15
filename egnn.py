f77998e1438742a4651529353ee4308337105000
xumwen <421269175@qq.com>
2020-04-15 19:56:19 +0800

    add relu in sample SagelaNet and delete a gcn layer in ClusterNet

--- egnn.py
+++ egnn.py
@@ -72,6 +72,8 @@ class SAGELANet(nn.Module):
         X = self.conv1(
             (X, X[:, res_n_id[0]]), edge_index[0], edge_feature=edge_weight[0].unsqueeze(-1), size=size[0])
 
+        X = F.leaky_relu(X)
+
         X = self.conv2(
             (X, X[:, res_n_id[1]]), edge_index[1], edge_feature=edge_weight[1].unsqueeze(-1), size=size[1])
 
@@ -82,9 +84,9 @@ class ClusterSAGELANet(nn.Module):
     def __init__(self, in_channels, out_channels, spatial_channels=16):
         super(ClusterSAGELANet, self).__init__()
         self.conv1 = SAGELA(
-            in_channels, spatial_channels, edge_channels=1, node_dim=1)
-        self.conv2 = SAGELA(
-            spatial_channels, out_channels, edge_channels=1, node_dim=1)
+            in_channels, out_channels, edge_channels=1, node_dim=1)
+        # self.conv2 = SAGELA(
+        #     spatial_channels, out_channels, edge_channels=1, node_dim=1)
 
     def forward(self, X, g):
         edge_index = g['edge_index']
@@ -92,7 +94,7 @@ class ClusterSAGELANet(nn.Module):
 
         X = self.conv1(X, edge_index, edge_feature=edge_weight.unsqueeze(-1))
     
-        X = self.conv2(X, edge_index, edge_feature=edge_weight.unsqueeze(-1))
+        # X = self.conv2(X, edge_index, edge_feature=edge_weight.unsqueeze(-1))
 
         return X
 
@@ -182,9 +184,9 @@ class ClusterGatedGCNNet(nn.Module):
     def __init__(self, in_channels, out_channels, spatial_channels=16):
         super(ClusterGatedGCNNet, self).__init__()
         self.conv1 = GatedGCN(
-            in_channels, spatial_channels, edge_channels=1, node_dim=1)
-        self.conv2 = GatedGCN(
-            spatial_channels, out_channels, edge_channels=1, node_dim=1)
+            in_channels, out_channels, edge_channels=1, node_dim=1)
+        # self.conv2 = GatedGCN(
+        #     spatial_channels, out_channels, edge_channels=1, node_dim=1)
 
     def forward(self, X, g):
         edge_index = g['edge_index']
@@ -192,6 +194,6 @@ class ClusterGatedGCNNet(nn.Module):
 
         X = self.conv1(X, edge_index, edge_feature=edge_weight.unsqueeze(-1))
 
-        X = self.conv2(X, edge_index, edge_feature=edge_weight.unsqueeze(-1))
+        # X = self.conv2(X, edge_index, edge_feature=edge_weight.unsqueeze(-1))
 
         return X
