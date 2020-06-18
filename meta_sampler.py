255ca1547a908e2904a283248a4274c18be53dd2
xumwen <421269175@qq.com>
2020-06-18 11:31:33 +0800

    pre-sample subgraphs in meta_sampler for dataloader

--- meta_sampler.py
+++ meta_sampler.py
@@ -23,10 +23,12 @@ class MetaSampler(object):
         self.sample_step = sample_step
         self.shuffle = shuffle
         self.random_sample = random_sample
+        self.subgraphs = []
         self.reset()
     
     def reset(self):
         self.node_visit = torch.zeros(self.num_nodes, dtype=torch.bool)
+        self.subgraphs = []
     
     def __get_init_nodes__(self, num_init_nodes=10):
         left_nodes = np.where(self.node_visit == 0)[0]
@@ -53,31 +55,8 @@ class MetaSampler(object):
         neighbor_id = np.where(tmp_node_mask & (node_mask==False) & (self.node_visit==False))[0]
 
         return neighbor_id
-    
-    def __pkg_state__(self, n_id, neighbor_id, state_vec):
-        state = {'n_id': n_id,
-                'neighbor_id': neighbor_id,
-                'state_vec': state_vec}
-        return state
 
-    def __get_init_state__(self):
-        n_id = self.__get_init_nodes__()
-        neighbor_id = self.__get_neighbor__(n_id)
-        state_vec = self.__get_state__(n_id, neighbor_id)
-        return __pkg_state__(n_id, neighbor_id, state_vec)
-    
-    def __get_state__(self, n_id, neighbor_id):
-        cent_emb = self.node_emb[n_id].sum(dim=0)
-        neighbor_emb = self.node_emb[neighbor_id].sum(dim=0)
-
-        state_vec = torch.cat([cent_emb, neighbor_emb], dim=0)
-        return state_vec
-
-    def step(self, state, action):
-        # load state info
-        n_id = state['n_id']
-        neighbor_id = state['neighbor_id']
-
+    def neighbor_sample(self, neighbor_id, action):
         # calculate kl-divergense to sample nodes
         mu1 = action[0]
         sigma1 = action[1]
@@ -87,53 +66,22 @@ class MetaSampler(object):
         kl_div = (sigma2 / sigma1).log() + (sigma1**2 + (mu1 - mu2)**2) / (2 * sigma2**2) - 0.5
         weight = torch.exp(-kl_div)
         sample_n_id = neighbor_id[torch.bernoulli(weight) == 1]
-        n_id = np.union1d(n_id, sample_n_id)
 
-        if len(n_id) < self.subgraph_nodes:
-            neighbor_id = self.__get_neighbor__(n_id)
-            state_vec = self.__get_state__(n_id, neighbor_id)
-        else:
-            neighbor_id = None
-            state_vec = None
+        return sample_n_id
 
-        return self.__pkg_state__(n_id, neighbor_id, state_vec)
-
-    def __produce_subgraph__(self):
+    def __produce_subgraph_by_nodes__(self, n_id):
         row, col = self.edge_index
         node_mask = torch.zeros(self.num_nodes, dtype=torch.bool)
         edge_mask = torch.zeros(row.size(0), dtype=torch.bool)
-        
-        for i in range(self.sample_step):
-            state = self.__get_init_state__()
-            n_id = state['n_id']
-            neighbor_id = state['neighbor_id']
-            s = state['state_vec']
 
-            if self.random_sample:
-                # random sample to warm start without node_emb
-                if subgraph_nodes >= self.num_nodes - self.node_visit.sum():
-                    sample_n_id = np.where(self.node_visit == False)[0]
-                else:
-                    num_left = subgraph_nodes - len(n_id)
-                    if len(neighbor_id) >= num_left:
-                        np.random.shuffle(neighbor_id)
-                        sample_n_id = neighbor_id[:num_left]
-                    else:
-                        sample_n_id = neighbor_id
-                n_id = np.union1d(n_id, sample_n_id)
-            else:
-                action, logp = self.policy.action(s)
-                state_prime = self.step(state, action)
-                n_id = state_prime['n_id']
-            
-            if len(n_id) >= self.subgraph_nodes:
-                break
-
+        # visited nodes update
         self.node_visit[n_id] = True
-        # return subgraph
+
+        # get edge
         node_mask[n_id] = True
         edge_mask = node_mask[row] & node_mask[col]
         e_id = np.where(edge_mask)[0]
+
         # edge_index reindex by n_id
         tmp = torch.empty(self.num_nodes, dtype=torch.long)
         tmp[n_id] = torch.arange(len(n_id))
@@ -141,11 +89,38 @@ class MetaSampler(object):
 
         return Data(edge_index=edge_index, n_id=n_id, e_id=e_id)
 
+    def __produce_subgraph__(self):
+        if self.num_nodes - self.node_visit.sum() <= subgraph_nodes:
+            # last subgraph
+            n_id = np.where(self.node_visit == False)[0]
+            return self.__produce_subgraph_by_nodes__(n_id)
+
+        # sample several steps
+        n_id = self.__get_init_nodes__()
+        for i in range(self.sample_step):
+            neighbor_id = self.__get_neighbor__(n_id)
+            if self.random_sample:
+                # random sample to warm start without node_emb
+                num_left = subgraph_nodes - len(n_id)
+                if len(neighbor_id) >= num_left:
+                    np.random.shuffle(neighbor_id)
+                    sample_n_id = neighbor_id[:num_left]
+                else:
+                    sample_n_id = neighbor_id
+            else:
+                action, logp = self.policy.action(s)
+                sample_n_id = self.neighbor_sample(neighbor_id, action)
+            
+            n_id = np.union1d(n_id, sample_n_id)
+            if len(n_id) >= self.subgraph_nodes:
+                break
+
+        return self.__produce_subgraph_by_nodes__(n_id)
+
     def __call__(self):
-        r"""Returns a generator of subgraph"""
         self.reset()
         while self.node_visit.sum() != self.num_nodes:
-            yield self.__produce_subgraph__()
+            self.subgraphs.append(self.__produce_subgraph__())
 
 
 class MetaSamplerDataset(IterableDataset):
@@ -178,7 +153,11 @@ class MetaSamplerDataset(IterableDataset):
         return g
 
     def __iter__(self):
-        for subgraph in self.meta_sampler():
+        # need pre-sampled subgraphs to avoid length inconsistency
+        self.meta_sampler()
+
+        for subgraph in self.meta_sampler.subgraphs:
+            print(subgraph.n_id)
             g = self.get_subgraph(subgraph)
             X, y = self.X[:, g['n_id']], self.y[:, g['n_id']]
             dataset_len = X.size(0)
@@ -197,7 +176,7 @@ class MetaSamplerDataset(IterableDataset):
     def get_length(self):
         length = 0
 
-        for _ in self.meta_sampler():
+        for _ in self.meta_sampler.subgraphs:
             num_samples_per_node = self.X.size(0)
             length += (num_samples_per_node +
                     self.batch_size - 1) // self.batch_size
